fb6e4990b17e18e2271db1d7e723a1cc1b0f92fb
Gustavo A Farias <farias.gustavo.a@gmail.com>
2020-09-22 10:58:59 -0300

    Fin seccion 3

---
+++ sockets/socket.js
@@ -0,0 +1,17 @@
+const { io } = require('../index');
+
+// Mensajes de socket
+io.on('connection', client => {
+    
+    console.log('Cliente conectado')
+
+    client.on('disconnect', () => { 
+        console.log('Cliente desconectado')
+     });
+
+     client.on('mensaje', ( payload ) => {
+         console.log('mensaje !!!!', payload);
+
+         io.emit( 'mensaje' , { admin: 'Mensaje recibido'})
+     } );
+  });
